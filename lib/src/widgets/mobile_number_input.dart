2138601efe5e46d6ef15388642f2c58369499070
amadejzr <amadej@jerlah.si>
2024-01-05 22:32:19 +0100

    Added countries

---
+++ lib/src/widgets/mobile_number_input.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class MobileNumberInput extends StatefulWidget {
+  const MobileNumberInput({super.key});
+
+  @override
+  State<MobileNumberInput> createState() => _MobileNumberInputState();
+}
+
+class _MobileNumberInputState extends State<MobileNumberInput> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
