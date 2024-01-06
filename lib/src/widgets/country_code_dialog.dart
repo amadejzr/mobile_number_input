c5eac19dfdcce48e795d8d26e337265560e0271f
amadejzr <amadej@jerlah.si>
2024-01-06 15:50:44 +0100

    ✨ Implement search functionality in 'CountryCodeDialog'
    Add text field for country search and debounce mechanism to optimize filter operation.
    
    Update UI to include search bar and display filtered country list dynamically.

--- lib/src/widgets/country_code_dialog.dart
+++ lib/src/widgets/country_code_dialog.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:mobile_number_input/src/models/country_code.dart';
 import 'package:mobile_number_input/src/providers/country_provider.dart';
+import 'dart:async';
 
 class CountryCodeDialog extends StatefulWidget {
   const CountryCodeDialog({
@@ -22,12 +23,15 @@ class CountryCodeDialog extends StatefulWidget {
 
 class _CountryCodeDialogState extends State<CountryCodeDialog> {
   late List<CountryCode> countryList;
-  int selectedIndex = 0;
+  final ValueNotifier<List<CountryCode>> _filteredCountryListNotifier = ValueNotifier([]);
+  final TextEditingController _searchController = TextEditingController();
+  Timer? _debounce;
 
   @override
   void initState() {
     super.initState();
     countryList = CountryProvider.getCountries();
+    _filteredCountryListNotifier.value = countryList;
   }
 
   void _handleCountryTap(CountryCode selectedCountry) {
@@ -35,33 +39,73 @@ class _CountryCodeDialogState extends State<CountryCodeDialog> {
     Navigator.of(context).pop();
   }
 
+  void _filterCountries(String enteredKeyword) {
+    if (_debounce?.isActive ?? false) _debounce!.cancel();
+    _debounce = Timer(const Duration(milliseconds: 500), () {
+      List<CountryCode> filteredList = enteredKeyword.isEmpty
+          ? countryList
+          : countryList
+              .where((country) => country.name.toLowerCase().contains(enteredKeyword.toLowerCase()))
+              .toList();
+      _filteredCountryListNotifier.value = filteredList;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Dialog(
       backgroundColor: widget.backgroundColor,
-      child: SizedBox(
-        width: double.maxFinite,
-        child: ListView.builder(
-          itemCount: countryList.length,
-          itemBuilder: (context, index) => ListTile(
-            onTap: () => _handleCountryTap(countryList[index]),
-            leading: Image.asset(
-              countryList[index].flagUri,
-              height: 20,
-              width: 20,
-              package: 'mobile_number_input',
-            ),
-            title: Text(
-              countryList[index].name,
-              style: widget.countryNameTextStyle,
-            ),
-            trailing: Text(
-              countryList[index].dialCode,
-              style: widget.countryCodeTextStyle,
+      child: Column(
+        children: [
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: TextField(
+              onChanged: _filterCountries,
+              controller: _searchController,
+              decoration: const InputDecoration(
+                hintText: 'Search',
+                prefixIcon: Icon(Icons.search),
+              ),
             ),
           ),
-        ),
+          Expanded(
+            child: ValueListenableBuilder<List<CountryCode>>(
+              valueListenable: _filteredCountryListNotifier,
+              builder: (context, filteredCountryList, child) {
+                return ListView.builder(
+                  itemCount: filteredCountryList.length,
+                  itemBuilder: (context, index) => ListTile(
+                    onTap: () => _handleCountryTap(filteredCountryList[index]),
+                    leading: Image.asset(
+                      filteredCountryList[index].flagUri,
+                      height: 20,
+                      width: 20,
+                      package: 'mobile_number_input',
+                    ),
+                    title: Text(
+                      filteredCountryList[index].name,
+                      style: widget.countryNameTextStyle,
+                    ),
+                    trailing: Text(
+                      filteredCountryList[index].dialCode,
+                      style: widget.countryCodeTextStyle,
+                    ),
+                  ),
+                );
+              },
+            ),
+          ),
+        ],
       ),
     );
   }
+
+  @override
+  void dispose() {
+    _debounce?.cancel();
+    _searchController.dispose();
+    _filteredCountryListNotifier.dispose();
+    super.dispose();
+  }
 }
+
